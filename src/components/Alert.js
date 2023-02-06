9ac4b7ad79565d44f0aa91475f9702bebe487a06
Sarthak Gupta <gauravsarthak19@gmail.com>
2023-02-06 14:42:25 +0530

    changes in alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -6,7 +6,6 @@ export default function Alert(props) {
     <div style={{height:'50px'}}>
    {props.alert && <div className="alert alert-success alert-dismissible fade show" role="alert">
    <strong> {props.alert.type} :{props.alert.msg} </strong>
-  <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>}
 </div>
 
